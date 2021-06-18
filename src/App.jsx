e74a4636a7d4052c3e7d07a121e69cb4c33d9bde
ARYAN <agg4807@gmail.com>
2021-06-18 16:23:03 +0530

    forms with two entry

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
 import React, { useState } from 'react';
-import SlotM from './Slotmach';
 
-
-const App =()=>{
-
-    const purple="purple";
-   const [bg,setBg]=useState(purple);
-  const [name,setName]=useState("click me");
-
-   const bgChange=()=>{
-//console.log("clicked");
-            let newcolor="#34954e";
-            setBg(newcolor);
-            setName("ph yes 🥇");
-    };
-const bgBack=()=>{
-    let newcolo="purple";
-    setBg(newcolo);
-    setName("aodos 🥈");
-
-}
-    return(
+const App =() => {
+const [name, setname]=useState("");
+const [passwor,setpass]=useState("");
+const [fullName,setfullName]=useState();
+const [lastName,setlastName]=useState("");
+    const inputEvent=(event)=>{
+        //console.log(event.target.value);
+            setname(event.target.value)
+    }
+    const inputEventTwo=(event)=>{
+        setpass(event.target.value)
+    }
+  const onSubmit=(event)=>{
+      event.preventDefault();
+      setfullName(name);
+        setlastName(passwor);
+  }
+    return(  
     <>
-    <div style={ {backgroundColor: bg} }>
-           <button onMouseEnter={bgChange}  onMouseLeave={ bgBack }> {name} </button>
-           </div>
-    </>
+    <form onSubmit={onSubmit}>
+        <div>
+        <h1>HI! {fullName} {lastName}</h1>
+        <input type="text" 
+        placeholder="enter your name" 
+        onChange={inputEvent} 
+        value={name}>
+         </input>
+
+        <input type="text" 
+        placeholder="enter your password" 
+        onChange={inputEventTwo} 
+             value={passwor}>
+             </input>
+
+        <button type='submit' >click me</button>
+        </div>
+        </form>  </>
     );
-}
+    }
+
+
 
 export default App;
