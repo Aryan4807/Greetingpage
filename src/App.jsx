7000be8b82274a8debb1a45fb1223de7ad514250
ARYAN <agg4807@gmail.com>
2021-06-22 00:25:55 +0530

    final form

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,104 @@
 import React, { useState } from 'react';
 
 const App =() => {
-const [name, setname]=useState("");
-const [passwor,setpass]=useState("");
-const [fullName,setfullName]=useState();
-const [lastName,setlastName]=useState("");
+const [Fullname, setFullname]=useState({
+    fName:"",
+    lName:"",
+    email:"",
+    Number:""
+});
+
     const inputEvent=(event)=>{
-        //console.log(event.target.value);
-            setname(event.target.value)
-    }
-    const inputEventTwo=(event)=>{
-        setpass(event.target.value)
-    }
+        console.log(event.target.value);
+        console.log(event.target.name);
+     
+           // setFullname(event.target.value)
+    // const value=event.target.value;
+    // const name=event.target.name;
+const {value,name}=event.target;
+    setFullname((prevalue)=>{
+   // console.log(prevalue);    
+            if(name==="fName")
+            {
+                return{
+                    fName: value,
+                    lName : prevalue.lName,
+                    email:prevalue.email,
+                    Number:prevalue.Number,
+            }
+            }
+            else  if(name==="lName")
+            {
+                return{
+                    fName: prevalue.fName,
+                    lName : value,
+                    email:prevalue.email,
+                    Number:prevalue.Number,
+            }
+            }
+            else  if(name==="email")
+            {
+                return{
+                    fName: prevalue.fName,
+                    lName :prevalue.lName ,
+                    email:value,
+                    Number:prevalue.Number,
+
+            }
+            }
+            else  if(name==="Number")
+            {
+                return{
+                    fName: prevalue.fName,
+                    lName : prevalue.lName,
+                    email:prevalue.email,
+                    Number:value,
+            }
+            }
+})
+        }
+  
   const onSubmit=(event)=>{
       event.preventDefault();
-      setfullName(name);
-        setlastName(passwor);
+      alert("form submitted");
   }
     return(  
     <>
     <form onSubmit={onSubmit}>
         <div>
-        <h1>HI! {fullName} {lastName}</h1>
+        <h1>HI! {Fullname.fName} {Fullname.lName}</h1>
+        <p>{Fullname.Number} {Fullname.email}</p>
         <input type="text" 
         placeholder="enter your name" 
+       name="fName"
         onChange={inputEvent} 
-        value={name}>
+       value={Fullname.fName}
+        >
          </input>
 
         <input type="text" 
         placeholder="enter your password" 
-        onChange={inputEventTwo} 
-             value={passwor}>
+       name="lName"
+        onChange={inputEvent} 
+             value={Fullname.lName}
+            >
              </input>
 
+             <input type="email" 
+             placeholder="enter your email" 
+            name="email"
+             onChange={inputEvent} 
+                  value={Fullname.email}
+                 >
+                  </input>
+                  
+        <input type="number" 
+        placeholder="enter your phone no" 
+       name="Number"
+        onChange={inputEvent} 
+             value={Fullname.Number}
+            >
+             </input>
         <button type='submit' >click me</button>
         </div>
         </form>  </>
